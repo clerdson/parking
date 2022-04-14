1b131d82a81ab3dc4374a526ce98b271a117fa77
clerdson <clerdson@gmail.com>
2022-04-14 00:52:04 -0300

    check connetction for internet

--- app/src/main/java/com/clerdsonjuca/drive/MainActivity.kt
+++ app/src/main/java/com/clerdsonjuca/drive/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.clerdsonjuca.drive
 
+import android.app.AlertDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.NetworkRequest.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.clerdsonjuca.drive.ui.MyPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,5 +30,45 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = fragmentAdapter
 
         Tab.setupWithViewPager(viewPager)
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            // network is available for use
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+            }
+
+            // Network capabilities have changed for the network
+            override fun onCapabilitiesChanged(
+                network: Network,
+                networkCapabilities: NetworkCapabilities
+            ) {
+                super.onCapabilitiesChanged(network, networkCapabilities)
+                val unmetered = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+            }
+
+            // lost network connection
+            override fun onLost(network: Network) {
+                super.onLost(network)
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.setTitle("Alerta.")
+                builder.setMessage("Este app precisa de internet")
+                builder.setPositiveButton("Sim"){dialog, which ->
+                    Toast.makeText(this@MainActivity,"ok.",Toast.LENGTH_SHORT).show()
+
+                }
+
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
+        val connectivityManager = getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
     }
 }
